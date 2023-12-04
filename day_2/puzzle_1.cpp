17e9a96686850984adc6c0c22bce3d54502f1e77
YatoNoragami1 <maximus17435@gmail.com>
2023-12-04 15:04:33 +0100

    add day_2

---
+++ day_2/puzzle_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <regex>
+#include <fstream>
+
+int main(){
+    const int red{12}; 
+    const int green{13};
+    const int blue{14};
+
+    const std::regex pattern{"\\b\w"};
+    std::ifstream puzzle_input("puzzle_input");
+    return 0;
+}
